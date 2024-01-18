f6681b3424c95526ca14fec21451f57a88bebdc4
bright-luminous <72394943+bright-luminous@users.noreply.github.com>
2024-01-19 00:19:31 +0700

    basic video upload functional

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,22 +1,44 @@
-import { Injectable } from '@nestjs/common';
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import { ConfigService } from '@nestjs/config';
+import { Injectable, Req, Res } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
 
 @Injectable()
 export class UploadService {
-  private readonly s3Client = new S3Client({
-    region: this.configService.getOrThrow('AWS_S3_REGION'),
+  AWS_S3_BUCKET = 'test-tti-video-bucket';
+  s3 = new AWS.S3({
+    accessKeyId: 'AKIARQUD5L5OQCTOJHF5',
+    secretAccessKey: '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',
   });
 
-  constructor(private readonly configService: ConfigService) {}
+  async uploadFile(file) {
+    console.log(file);
+    const { originalname } = file;
 
-  async upload(fileName: string, file: Buffer) {
-    await this.s3Client.send(
-      new PutObjectCommand({
-        Bucket: 'nestjs-uploader',
-        Key: fileName,
-        Body: file,
-      }),
+    return await this.s3_upload(
+      file.buffer,
+      this.AWS_S3_BUCKET,
+      originalname,
+      file.mimetype,
     );
+  }
+
+  async s3_upload(file, bucket, name, mimetype) {
+    const params = {
+      Bucket: bucket,
+      Key: String(name),
+      Body: file,
+      ACL: 'public-read',
+      ContentType: mimetype,
+      ContentDisposition: 'inline',
+      CreateBucketConfiguration: {
+        LocationConstraint: 'ap-south-1',
+      },
+    };
+
+    try {
+      let s3Response = await this.s3.upload(params).promise();
+      return s3Response;
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
